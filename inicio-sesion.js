03c8f04c5fa2c3053502b268ab9485977802eab7
Luis Mayayo <a25597@svalero.com>
2024-05-02 13:06:57 +0200

    ajustado log in

--- inicio-sesion.js
+++ inicio-sesion.js
@@ -1,16 +1,31 @@
 // Obtener el modal y el botón de cierre
-var modal = document.getElementById('loginModal');
-var btn = document.getElementById("loginBtn");
-var span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById('loginModal');
+const btn = document.getElementById("loginBtn");
+const span = document.querySelector(".close");
 
-// Abrir el modal cuando se hace clic en el botón
-btn.onclick = function () {
+// Función para abrir el modal
+function openModal() {
     modal.style.display = "block";
 }
 
-// Cerrar el modal cuando se hace clic en el botón de cierre o fuera del modal
-window.onclick = function (event) {
-    if (event.target == modal || event.target == span) {
-        modal.style.display = "none";
-    }
+// Función para cerrar el modal
+function closeModal() {
+    modal.style.display = "none";
 }
+
+// Abrir el modal cuando se hace clic en el botón
+btn.addEventListener("click", openModal);
+
+// Cerrar el modal cuando se hace clic en el botón de cierre o fuera del modal
+window.addEventListener("click", function(event) {
+    if (event.target === modal || event.target === span) {
+        closeModal();
+    }
+});
+
+// Cerrar el modal cuando se presiona la tecla Escape
+window.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+        closeModal();
+    }
+});
