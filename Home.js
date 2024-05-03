92ab44dcea78c1692115b9d6a22d0576f5ee5a01
Luis Mayayo <a25597@svalero.com>
2024-05-03 18:37:49 +0200

    cambiado idioma de la pagina(da un error con el español))

---
+++ Home.js
@@ -0,0 +1,7 @@
+function cambiarIdioma(idioma) {
+    if (idioma === 'es') {
+        window.location.href = 'home.html'; 
+    } else if (idioma === 'en') {
+        window.location.href = 'home_ingles.html';
+    }
+}
