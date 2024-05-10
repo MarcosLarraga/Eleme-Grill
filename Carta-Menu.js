3c082393094c9f377818a391e5863ffa5fbed853
Luis Mayayo <a25597@svalero.com>
2024-05-10 21:21:58 +0200

    ARRGLADO EL ORDEN DEL CODIGO

--- Carta-Menu.js
+++ Carta-Menu.js
@@ -1,25 +1,25 @@
 function openModal(modalId) {
     var modal = document.getElementById(modalId);
     modal.style.display = "block";
-  }
-  
-  function closeModal(modalId) {
+}
+
+function closeModal(modalId) {
     var modal = document.getElementById(modalId);
     modal.style.display = "none";
-  }
-  
-  // Cierra el modal cuando se hace clic en la X
-  var closeButtons = document.querySelectorAll(".close");
-  closeButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
-      var modalId = this.parentElement.parentElement.id;
-      closeModal(modalId);
+}
+
+// Cierra el modal cuando se hace clic en la X
+var closeButtons = document.querySelectorAll(".close");
+closeButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        var modalId = this.parentElement.parentElement.id;
+        closeModal(modalId);
     });
-  });
-  
-  // Cierra el modal cuando se hace clic fuera del contenido
-  window.onclick = function(event) {
+});
+
+// Cierra el modal cuando se hace clic fuera del contenido
+window.onclick = function (event) {
     if (event.target.classList.contains("modal")) {
-      closeModal(event.target.id);
+        closeModal(event.target.id);
     }
-  };
+};
