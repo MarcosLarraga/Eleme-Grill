70182af564bb1a8801dd36e1c5f6de186e8f097e
MarcosLarraga04 <a25596@svalero.com>
2024-05-13 19:40:19 +0200

    responsive hecho

--- Home.js
+++ Home.js
@@ -1,15 +1,20 @@
-function cambiarIdioma(idioma) {
-    if (idioma === 'es') {
-        window.location.href = 'home.html'; 
-    } else if (idioma === 'en') {
-        window.location.href = 'home_ingles.html';
-    }
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu(event) {
+  this.classList.toggle('is-active');
+  var menuContainer = document.querySelector(".menuppal");
+  menuContainer.classList.toggle("is_active");
+
+  if (menuContainer.classList.contains("is_active")) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+
+  event.preventDefault();
 }
 
-document.getElementById('Espanol').addEventListener('click', function() {
-    cambiarIdioma('es');
-});
-
-document.getElementById('Ingles').addEventListener('click', function() {
-    cambiarIdioma('en');
-});
+// event
+menu.addEventListener('click', toggleMenu, false);
