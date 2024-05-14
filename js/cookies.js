81d5fd5ca082581c88e266def4ed7d58f6e98da5
MarcosLarraga04 <a25596@svalero.com>
2024-05-14 21:41:35 +0200

    mergear politica y cookies falta arreglar cookies

--- js/cookies.js
+++ js/cookies.js
@@ -23,3 +23,24 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.removeItem("cookiesAccepted");
     });
 });
+
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu(event) {
+  this.classList.toggle('is-active');
+  var menuContainer = document.querySelector(".menuppal");
+  menuContainer.classList.toggle("is_active");
+
+  if (menuContainer.classList.contains("is_active")) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
