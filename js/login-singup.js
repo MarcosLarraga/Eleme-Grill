c2d9120008a40749c34e4f9af2062a97aa2a371c
MarcosLarraga04 <a25596@svalero.com>
2024-05-15 17:48:23 +0200

    falta arreglar login y terminar carrito

--- js/login-singup.js
+++ js/login-singup.js
@@ -1,7 +1,7 @@
 const closeButton = document.getElementsByClassName('close-btn')[0];
         
         closeButton.onclick = function() {
-            window.location.href = '../h'; // Redirige al usuario a la página principal o donde desees
+            window.location.href = 'home.html'; // Redirige al usuario a la página principal o donde desees
         }
 
         document.getElementById('formAcceso').onsubmit = function(event) {
@@ -10,13 +10,9 @@ const closeButton = document.getElementsByClassName('close-btn')[0];
             const password = document.getElementById('userPassword').value;
             const correctEmailAdmin = 'admin@gmail.com'; // Correo del administrador
             const correctPasswordAdmin = 'admin123'; // Contraseña del administrador
-            const correctEmailEmpleado = 'empleadoBV@gmail.com'; // Correo del empleado
-            const correctPasswordEmpleado = '1234'; // Contraseña del empleado
         
             if (email === correctEmailAdmin && password === correctPasswordAdmin) {
                 window.location.href = 'zona_privada.html'; // Redirige a la página de administrador
-            } else if (email === correctEmailEmpleado && password === correctPasswordEmpleado) {
-                window.location.href = 'zona_privada_empleados.html'; // Redirige a la página de empleado
             } else {
                 alert('Acceso denegado.');
             }
