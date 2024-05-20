8a0fe7a8ce00e7f01c3900e630ab170c4231cb5d
Luis Mayayo <a25597@svalero.com>
2024-05-20 20:38:40 +0200

    retoque archivos

--- js/bebidas.js
+++ js/bebidas.js
@@ -16,28 +16,3 @@ function toggleMenu(event) {
 }
 
 menu.addEventListener('click', toggleMenu, false);
-
-/*modal*/
-function openModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "block";
-}
-
-function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
-
-var closeButtons = document.querySelectorAll(".close");
-closeButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        var modalId = this.parentElement.parentElement.id;
-        closeModal(modalId);
-    });
-});
-
-window.onclick = function (event) {
-    if (event.target.classList.contains("modal")) {
-        closeModal(event.target.id);
-    }
-};
