5785549fa5e666e91e834300366f1b5baa4748a3
MarcosLarraga04 <a25596@svalero.com>
2024-05-21 21:49:51 +0200

    clase anton

--- js/login-singup.js
+++ js/login-singup.js
@@ -14,6 +14,24 @@ document.getElementById('formAcceso').onsubmit = function (evento) {
     if (correo === correoCorrectoAdmin && contrasena === contrasenaCorrectaAdmin) {
         window.location.href = 'ZonaPrivada.html';
     } else {
-        alert('Acceso denegado.');
+    //    alert('Acceso denegado.');
     }
+
+    let Cliente = {
+        CL_CLIENTE_ID : "1",
+        CL_NOMBRE : "Pepa",
+        CL_APELLIDO : "Lópeza",
+        CL_DIRECCIÓN : "Calle Barranca 69",
+        CL_FE_NACIMIENTO : "01/01/1975",
+        CL_TELÉFONO : "33-33",
+        CL_EMAIL : "la33vallegar@alonsojodete.ya"    
+    }
+    const url = "http://localhost:8080/ELEME-GRILL/Controller?ACTION=CLIENTE.ADD";
+    fetch(url, {
+        method: "POST", // or 'PUT'
+        body: JSON.stringify(Cliente), // data can be `string` or {object}!        
+      }).then(function (response) {
+        alert (response.json());
+        return response.json();
+      }); 
 }
