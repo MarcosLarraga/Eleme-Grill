b61266112ca99722198a4c3df184e45c61ec7e8b
Luis Mayayo <a25597@svalero.com>
2024-05-23 14:23:53 +0200

    empezado fetch employees

--- js/zonaprivada.js
+++ js/zonaprivada.js
@@ -27,3 +27,41 @@ function loadContent(type) {
             document.getElementById('content').textContent = 'Failed to load content';
         });
 }
+
+const urlEmployees = '';
+
+const fetchEmployees = async ()=> {
+    try{
+        const result = await fetch(urlEmployees);
+        const data = await result.json();
+        console.log('Estos son los empleados que hay en la API:', data);
+        printEmployees(data);
+    }catch(error){
+        console.log('Error al extraer datos con la API', error);
+    }
+}
+
+const printEmployees = (employees) => {
+    const table = document.getElementById('tabla-empleados');
+    const tbody = table.querySelector('tbody');
+    table.style.display= 'table';
+
+    employees.forEach(employee => {
+        const {
+            
+        } =employee;
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        `;
+        tbody.appendChild(row);
+    })
+}
+fetchEmployees();
