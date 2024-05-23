f41f3e15ca83463e7d1161781db65a1be6156431
Luis Mayayo <a25597@svalero.com>
2024-05-23 12:43:25 +0200

    probando fetch de carta

--- js/carta.js
+++ js/carta.js
@@ -1,3 +1,52 @@
+console.log('Hola mundo desde script.js');
+
+// URL de la API para obtener los datos
+const url = 'https://pokeapi.co/api/v2/pokemon/54'; // Ajusta esta URL a la correcta para tu API
+
+const fetchCategories = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log('Datos de la API ----->', data);
+
+        // categorias estan en array en el objeto `data`
+        const categories = [
+            data, // Hamburguesas
+            data, // Para Compartir
+            data, // Bebidas
+            data  // Postres
+        ];
+
+        categories.forEach((item, index) => {
+            updateCategory(item, index + 1);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+// Función para actualizar cada categoría con los datos obtenidos
+const updateCategory = (item, categoryId) => {
+    const {
+        name,
+        sprites: {
+            other: {
+                home: { front_default: imgItemFront }
+            }
+        }
+    } = item;
+
+    const category = document.getElementById(`category-${categoryId}`);
+    if (category) {
+        const img = category.querySelector('img');
+        img.src = imgItemFront;
+        img.alt = name;
+    }
+};
+
+fetchCategories();
+
+//hamburger
 var menu = document.querySelector('.hamburger');
 
 function toggleMenu(event) {
