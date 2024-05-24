57a2e4fd7122e822b695da3705cc167a7eb00a4a
Luis Mayayo <a25597@svalero.com>
2024-05-24 18:25:42 +0200

    cambiado formato de pago

--- js/carrito.js
+++ js/carrito.js
@@ -61,3 +61,49 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'resumen-pedido.html';
     });
 });
+
+const urlEmployees = 'http://localhost:8080/ELEME-GRILL/Controller?ACTION=EMPLEADO.FIND_ALL';
+
+const fetchEmployees = async ()=> {
+    try{
+        const result = await fetch(urlEmployees);
+        const data = await result.json();
+        console.log('Estos son los empleados que hay en la API:', data);
+        printEmployees(data);
+    }catch(error){
+        console.log('Error al extraer datos con la API', error);
+        
+    }
+}
+
+const printEmployees = (employees) => {
+    const table = document.getElementById('tabla-empleados');
+    const tbody = table.querySelector('tbody');
+    table.style.display= 'table';
+
+    employees.forEach(employee => {
+        const {
+            EM_EMPLEADO_ID,
+            EM_NOMBRE,
+            EM_APELLIDO,
+            EM_DIRECCION,
+            EM_TELEFONO,
+            EM_EMAIL,
+            EM_ZONA_PRIVADA_ID,
+        } = employee;
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${EM_EMPLEADO_ID}</td>
+        <td>${EM_NOMBRE}</td>
+        <td>${EM_APELLIDO}</td>
+        <td>${EM_DIRECCION}</td>
+        <td>${EM_TELEFONO}</td>
+        <td>${EM_EMAIL}</td>
+        <td>${EM_ZONA_PRIVADA_ID}</td>
+        `;
+        tbody.appendChild(row);
+    })
+}
+fetchEmployees();
