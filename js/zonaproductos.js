185a76ceb4e9b0d86fef90838334160aa49719a0
Luis Mayayo <a25597@svalero.com>
2024-05-24 16:52:00 +0200

    añadido fetch a demas js

--- js/zonaproductos.js
+++ js/zonaproductos.js
@@ -4,3 +4,47 @@ const botonCerrar = document.getElementsByClassName('close-btn')[0];
 botonCerrar.onclick = function () {
     window.location.href = 'zonaprivada.html';
 }
+
+const urlEmployees = 'http://localhost:8080/ELEME-GRILL/Controller?ACTION=PRODUCTOS.FIND_ALL';
+
+const fetchEmployees = async () => {
+    try {
+        const result = await fetch(urlEmployees);
+        const data = await result.json();
+        console.log('Estos son los empleados que hay en la API:', data);
+        printEmployees(data);
+    } catch (error) {
+        console.log('Error al extraer datos con la API', error);
+    }
+};
+
+const printEmployees = (employees) => {
+    const table = document.getElementById('tabla-empleados');
+    const tbody = table.querySelector('tbody');
+    tbody.innerHTML = '';
+
+    employees.forEach(employee => {
+        const {
+            CL_CLIENTE_ID,
+            CL_NOMBRE,
+            CL_APELLIDO,
+            CL_DIRECCION,
+            CL_TELEFONO,
+            CL_EMAIL,
+            CL_CONTRASENA,
+        } = employee;
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${CL_CLIENTE_ID}</td>
+        <td>${CL_NOMBRE}</td>
+        <td>${CL_APELLIDO}</td>
+        <td>${CL_DIRECCION}</td>
+        <td>${CL_TELEFONO}</td>
+        <td>${CL_EMAIL}</td>
+        <td>${CL_CONTRASENA}</td>
+        `;
+        tbody.appendChild(row);
+    });
+};
