409a23280c92c6fa5cc9b91fcfed76996f39afc4
Luis Mayayo <a25597@svalero.com>
2024-05-28 21:21:36 +0200

    agregar cookies en home

--- js/home.js
+++ js/home.js
@@ -1,3 +1,25 @@
+window.onload = function () {
+  document.getElementById("cookieAlert").classList.add("show");
+};
+
+function acceptCookies() {
+  // Set cookie here if necessary
+  document.getElementById("cookieAlert").style.transition = "bottom 0.5s ease";
+  document.getElementById("cookieAlert").style.bottom = "-100%";
+}
+
+function rejectCookies() {
+  // Set cookie here if necessary
+  document.getElementById("cookieAlert").style.transition = "bottom 0.5s ease";
+  document.getElementById("cookieAlert").style.bottom = "-100%";
+  setTimeout(showCookieAlert, 1000); // Show alert again after x seconds
+}
+
+function showCookieAlert() {
+  document.getElementById("cookieAlert").style.transition = "bottom 0.5s ease";
+  document.getElementById("cookieAlert").style.bottom = "0";
+}
+
 var menu = document.querySelector('.hamburger');
 
 
@@ -15,6 +37,4 @@ function toggleMenu(event) {
   event.preventDefault();
 }
 
-
 menu.addEventListener('click', toggleMenu, false);
-
